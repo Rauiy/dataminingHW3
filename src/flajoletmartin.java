8b7701770d5d1683a06c15c784c41bd986cd8f04
Steven Ly <Rauiy@bitbucket.org>
2016-11-24 12:01:18 +0100

    added some functions

--- src/flajoletmartin.java
+++ src/flajoletmartin.java
@@ -1,6 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class flajoletmartin {
 
+    public static void main(String[] args){
+        flajoletmartin fm = new flajoletmartin();
+        fm.readfile("out.actor-collaboration");
+    }
+
+    public void readfile(String filename){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+            String line;
+            while((line = br.readLine()) != null){
+                System.out.println("Estimate: " + fm(line));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public int fm(String input){
         int zeros = r(input.hashCode());
