1cb96005161935f38c9b2e8a0ec99cebfa6a5ed4
Steven Ly <Rauiy@bitbucket.org>
2016-11-24 11:46:54 +0100

    Readded everything

--- src/flajoletmartin.java
+++ src/flajoletmartin.java
@@ -1,4 +1,25 @@
 
 public class flajoletmartin {
 
+
+    public int fm(String input){
+        int zeros = r(input.hashCode());
+        if(zeros > max)
+            max = zeros;
+
+        return estimate(max);
+    }
+
+    public int r(int h){
+        int i;
+        for(i = 0; h%2 == 0; i++)
+            h = h>>1;
+        return i;
+    }
+
+    int max = -1;
+
+    public int estimate(int r){
+        return (int) Math.pow(2, r);
+    }
 }
