dd13e59c794bea99c8d27a2958b222d53b122b3b
Steven Ly <Rauiy@bitbucket.org>
2016-11-28 16:14:02 +0100

    changes the solution to rightmost bits

--- src/hyperloglog.java
+++ src/hyperloglog.java
@@ -11,7 +11,7 @@ import java.util.Random;
 
 
 public class hyperloglog{
-    static final int b = 2, bits = 32-b;
+    static final int b = 5, bits = 32-b;
     static int mask = 3, rmask = 1073741823;; // The amount of bits we use
 
 
@@ -22,16 +22,21 @@ public class hyperloglog{
         rmask = 0;
         for(int i = 0; i < bits;i++)
             rmask = (rmask << 1) + 1;
- /*
+/*
         Random r = new Random();
         int[] M = new int[(int)Math.pow(2,b)];
         for(int i = 0; i < M.length; i++)
             M[i] = Integer.MIN_VALUE;
 
+        int[] random = new int[200000];
+        for(int i = 0; i < random.length; i++){
+            random[i] = r.nextInt();
+        }
+
         hyperloglog hll = new hyperloglog();
         HashSet<Integer> set = new HashSet<>();
-        for(int i = 0; i < 9999999; i++){
-            int n = r.nextInt(Integer.MAX_VALUE);
+        for(int i = 0; i < 10000000; i++){
+            int n = random[r.nextInt(200000)];
             //System.out.println("Bits: " + hll.r(n,bits) + " nr: " + n + " address: " + hll.address(n));
             //System.out.println("Binary form: " + Integer.toBinaryString(n) + " size: " + Integer.toBinaryString(n).length());
             M = hll.add(M, n+"");
@@ -39,9 +44,12 @@ public class hyperloglog{
         }
         System.out.println("Harmonic mean: " + hll.harmonic(M));
         System.out.println("Real distinct: " + set.size());
-*/
+        */
         hyperloglog hll = new hyperloglog();
         hll.streamFile("out.actor-collaboration");
+
+
+
     }
 
     public void streamFile(String filename){
@@ -66,23 +74,25 @@ public class hyperloglog{
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        for(int i: M)
+            System.out.println("M size: " + i);
     }
 
     public int[] add(int[] M, HashSet set, String[] items){
         for(String s: items) {
-            M = add(M, s);
-            set.add(s);
+            M = add(M, set,s);
         }
 
         return M;
     }
-    public int[] add(int[] M, String item){
+    public int[] add(int[] M, HashSet set, String item){
         int x = item.hashCode();
-        int j = address(x);
-        int b = r(x,bits);
+        int j = addressR(x);
+        int b = rR(x,bits);
         /*if(b > M[j])
             System.out.println("New max for: " + j);*/
-
+        set.add(x);
         M[j] = Math.max(b,M[j]);
 
         return M;
@@ -105,14 +115,46 @@ public class hyperloglog{
         return bits - Integer.toBinaryString(hash).length() + 1;
     }
 
+    public int addressR(int hash){
+        return (hash&mask); // 3 = 0b11
+    }
+
+    public int rR(int hash, int bits){
+        //hash = hash & rmask;
+        hash = hash >> b;
+
+        int i = 1;
+        while(hash%2 == 0){
+            hash = hash >> 1;
+            i++;
+        }
+
+        return i;
+    }
+
     public double harmonic(int[] M){
-        return (M.length/sum(M));
+        double alfa = alpha(M);
+        double p = M.length;
+
+        return alfa*p*p*sum(M);
     }
 
     public double sum(int[] M){
         double sum = 0;
         for(int i: M)
-            sum+=(1/Math.pow(2,i));
-        return sum;
+            sum+=(Math.pow(2,-i));
+        return 1/sum;
+    }
+
+    public double alpha(int[] M){
+        /* Estimation or precalculated alfas*/
+        switch (M.length){
+            case(2): return 1/(2*1.42371);
+            case(4): return 0.532434;
+            case(8): return 0.635576;
+            case(16): return 0.673;
+            default:
+                return 0.7213/(1+1.079/M.length); //Estimation
+        }
     }
 }
