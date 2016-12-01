a5f3dcd5a134c4023368047984989b416f01b724
Steven Ly <Rauiy@bitbucket.org>
2016-12-01 16:02:15 +0100

    fixed the damn shit hyperball now

--- src/hyperloglog.java
+++ src/hyperloglog.java
@@ -2,8 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Random;
+import java.util.*;
 
 /**
  * Created by Steven on 2016-11-24.
@@ -11,7 +10,7 @@ import java.util.Random;
 
 
 public class hyperloglog{
-    static final int b = 5, bits = 32-b;
+    static final int b = 2, bits = 32-b;
     static int mask = 3, rmask = 1073741823;; // The amount of bits we use
 
 
@@ -46,27 +45,75 @@ public class hyperloglog{
         System.out.println("Real distinct: " + set.size());
         */
         hyperloglog hll = new hyperloglog();
-        hll.streamFile("out.actor-collaboration");
-
+        hll.streamFile("out.com-amazon");
+        /*int m = (int)Math.pow(2,b);
+        for(int i=1; i < max+1; i++){
+            for(int j = 0; j < m; j++){
+                System.out.print(c[i][j] + " ");
+            }
+            System.out.println();
+        }*/
+        int[][] sol = hll.hyperBall(c);
+        /*for(int i=1; i < max+1; i++){
+            for(int j = 0; j < m; j++){
+                System.out.print(sol[i][j] + " ");
+            }
+            System.out.println();
+        }*/
 
 
     }
-
+    static HashMap<String, node> nodes;
+    static int[][] c;
+    static int max = 0;
     public void streamFile(String filename){
-        int[] M = new int[(int)Math.pow(2,b)];
-        for(int i = 0; i < M.length; i++)
-            M[i] = Integer.MIN_VALUE;
+        int m = (int)Math.pow(2,b);
+       // int[] M = new int[(int)Math.pow(2,b)];
+       // for(int i = 0; i < M.length; i++)
+        //   M[i] = Integer.MIN_VALUE;
         hyperloglog hll = new hyperloglog();
-        HashSet<String> set = new HashSet<>();
+        //HashSet<String> set = new HashSet<>();
+        nodes = new HashMap<>();
         try {
             BufferedReader br = new BufferedReader(new FileReader(filename));
             String line;
-            int i = 0;
+
             while((line = br.readLine()) != null){
-                M = add(M, set, line.split(" "));
+                if(line.indexOf("%") != -1)
+                    continue;
+                //M = add(M, set, line.split(" "));
                 //list.add(line);
-                if(i++%100000 == 0)
-                    System.out.println("Estimate: " + hll.harmonic(M) + " Real: " + set.size());//fm(line.split(" ")));
+                String[] n = line.split(" ");
+
+                node n1 = nodes.get(n[0]);
+                node n2 = nodes.get(n[1]);
+
+                if(n1 == null && n2 == null){
+                    n1 = new node(n[0], m);
+                    n2 = new node(n[1], m);
+
+                    nodes.put(n[0],n1);
+                    nodes.put(n[1],n2);
+                }
+                else if( n1 == null){
+                    n1 = new node(n[0], m);
+                    nodes.put(n[0],n1);
+                }
+                else if( n2 == null){
+                    n2 = new node(n[1], m);
+                    nodes.put(n[1],n2);
+                }
+
+                n1.add(n2);
+                //n2.add(n1);
+                n1.M = hll.add(n1.M, n[1]);
+                //n2.M = hll.add(n2.M, n[0]);
+
+                if(n1.id > max) max = n1.id;
+                if(n2.id > max) max = n2.id;
+
+                //if(i++%100000 == 0)
+                  //  System.out.println("Estimate: " + hll.harmonic(M) + " Real: " + set.size());//fm(line.split(" ")));
             }
 
         } catch (FileNotFoundException e) {
@@ -75,24 +122,29 @@ public class hyperloglog{
             e.printStackTrace();
         }
 
-        for(int i: M)
-            System.out.println("M size: " + i);
+        c = new int[max+1][];
+        System.out.println(max+1);
+        for(node v: nodes.values()) {
+            c[v.id] = v.M;
+        }
+
+        //for(int i: M)
+        //    System.out.println("M size: " + i);
     }
 
-    public int[] add(int[] M, HashSet set, String[] items){
+    public int[] add(int[] M, String[] items){
         for(String s: items) {
-            M = add(M, set,s);
+            M = add(M,s);
         }
 
         return M;
     }
-    public int[] add(int[] M, HashSet set, String item){
+    public int[] add(int[] M, String item){
         int x = item.hashCode();
         int j = addressR(x);
         int b = rR(x,bits);
         /*if(b > M[j])
             System.out.println("New max for: " + j);*/
-        set.add(x);
         M[j] = Math.max(b,M[j]);
 
         return M;
@@ -159,13 +211,81 @@ public class hyperloglog{
     }
 
     public int[] union(int[] M, int[] N){
+        int[] a = new int[M.length];
         for(int i = 0; i < N.length; i++){
-            M[i] = Math.max(M[i], N[i]);
+            a[i] = Math.max(M[i], N[i]);
         }
-        return M;
+        return a;
     }
 
-    public void hyperBall(){
+    public int[][] hyperBall(int[][] c){
+        int[][] recent = c.clone();
+        int  t= 0;
+        boolean changed = true;
 
+        while(changed) {
+            changed = false;
+            for (node v : nodes.values()) {
+
+                int[] a = c[v.id].clone();
+
+                for (node w : v.nodes) {
+
+                    //System.out.println("edge"+ v.id + " " + w.id);
+                    a = union(a, c[w.id]);
+
+                    changed = !compare(a, c[v.id]);
+                }
+                recent[v.id] = a;
+            }
+            System.out.println(t + " changed: " + changed);
+            /*System.out.println("~~~~~~~~~~");
+            int m = (int)Math.pow(2,b);
+            for(int i=1; i < max+1; i++){
+                for(int j = 0; j < m; j++){
+                    System.out.print(recent[i][j] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println("~~~~~~~~~~");*/
+            c = recent.clone();
+            t++;
+        }
+
+
+
+        return recent;
     }
+
+    boolean compare(int[] a, int[] b){
+        //System.out.println("Comparing");
+        for(int i = 0; i < a.length; i++) {
+            //System.out.println("Comparing " + a[i] + " to " + b[i] + " " + (a[i] == b[i]));
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+
+class node{
+    public node(String id, int m){
+        this.id = Integer.parseInt(id);
+        nodes = new ArrayList<>();
+        M = new int[m];
+        for(int i = 0; i < M.length; i++)
+            M[i] = 0;
+    }
+
+    public void add(node n){
+        nodes.add(n);
+    }
+
+    public int id;
+    public int[] M;
+    List<node> nodes;
+
 }
